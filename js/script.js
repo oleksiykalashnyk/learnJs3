bc94bd8b4c95331f649483e5a3176ae2962805ad
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-11-11 05:05:05 +0100

    finish fetch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 'use strict';
 
-//Learn FETCH
+//Learn Fetch API
+
+//Easy type request for server vs fetch
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(ax => ax.json())
+    .then(d => console.log(d));
+
+//But if need POST "data" or other type requests need use this
+fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        body: JSON.stringify({
+            id: "1"
+        }),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
