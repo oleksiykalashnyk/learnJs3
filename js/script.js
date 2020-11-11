dd4450ba2583839a7919a3d7ccf7e2d4b00b3f4e
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-11-11 01:38:57 +0100

    Finish learn this

--- js/script.js
+++ js/script.js
@@ -2,3 +2,36 @@
 
 //Learn AJAX
 
+const
+    inputRub = document.querySelector("#rub"),
+    inputUsd = document.querySelector("#usd");
+
+inputRub.addEventListener("input", () => {
+
+    const request = new XMLHttpRequest();
+
+    request.open("GET", "js/current.json");
+    request.setRequestHeader('Content-type', "application/json", "charset=utf-8");
+    request.send();
+
+
+    //First type add event for request "rearystatechange"
+    request.addEventListener("readystatechange", () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Problem in server";
+        }
+    });
+
+    //Second type listen event for request => "load"
+    request.addEventListener("load", () => {
+        if (request.readyState === 4) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Problem in server";
+        }
+    });
+});
